f54eb1959d34a02d0c026ddf8c8ecda8cc675ce6
Shayan-meo <159838275+Shayan-meo@users.noreply.github.com>
2024-06-06 20:53:28 +0900

    Add files via upload

---
+++ Q04.js
@@ -0,0 +1,6 @@
+"use strict";
+// 4 .Famous Quote: Find a quote from a famous person you admire. Print the quote and the name
+//  of its author. Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.
+let name1 = ("Harzat Ali Said");
+console.log(`${name1} “ Each Person's To Grief Is Acording To His Courage ”`);
---
+++ Q07.js
@@ -0,0 +1,10 @@
+"use strict";
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 
+// 8. Be sure to enclose your operations in print statements to see the results.
+// No 1 :   Addition
+let a = 10;
+let b = 8;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
