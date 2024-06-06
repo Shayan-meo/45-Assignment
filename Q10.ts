9d944506127a4f93904f66bf41bd21bd3bdd1679
Shayan-meo <159838275+Shayan-meo@users.noreply.github.com>
2024-06-06 20:57:58 +0900

    Add files via upload

---
+++ Q10.ts
@@ -0,0 +1,20 @@
+
+    // 10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+    // If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. 
+    // Then write one sentence describing what the program does.
+
+         
+    
+
+//  let name1 : string ="Shayan"
+//  let name2 : string = "Ahsan"
+  
+//  let A : number = 12
+//  let B : number = 25
+//  console.log(`$[name1] $[name2] $[A] $[B]`);
+
+
+
+ 
+
+
